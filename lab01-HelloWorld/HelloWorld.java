0a8ef8efd769b9f725102d666e7c21f6f2e731ce
jswiecicki <joseph.swiecicki@gmail.com>
2015-01-23 16:22:40 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//define a class
+public class HelloWorld {
+    
+    //main method
+    public static void main(String[] args) {
+        
+        // print out the hello world message surrounded by a box
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!   #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        
+    }
+}
+    
