5177a7dd9707e950e734595a4b711b9a46d100e8
jswiecicki <joseph.swiecicki@gmail.com>
2015-01-26 22:12:49 +0000

    updated HelloWorld and created hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+// CSE 002-111 Spring 2015 Prof. Brian Chen
+// Joseph Swiecicki 
+// 26 Jan 2015
+
+//define a class
+public class WelcomeClass {
+    
+    //main method
+    public static void main(String[] args) {
+        
+        // print out the welcome message surrounded by a box
+        System.out.println("  ----------- ");
+        System.out.println("  | WELCOME | ");
+        System.out.println("  ----------- ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        // '\\' for a backslash
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-J--P--S--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("In the past 3 years I have been on an impromptu Nordic landscape expedition, visiting Finland, Iceland and Norway.");
+
+    }
+}
+
